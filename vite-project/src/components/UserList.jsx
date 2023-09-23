b6b4bae447cae0345fcef5cbb73d48913890a8dd
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-09-23 20:04:06 -0300

    Actualizacion UserState

--- vite-project/src/components/UserList.jsx
+++ vite-project/src/components/UserList.jsx
@@ -2,12 +2,28 @@ import { useContext } from "react"
 import UsersContext from "../context/UsersContext"
 
 export const UserList = () => {
+    // ctx se coloca por convencion en React por entenderse como CONTEXTO
+    // useContext es un Hook
+    const globalContext = useContext(UsersContext)
+    console.log(globalContext)
 
-    const ctx = useContext()
+    const { users } = globalContext.initialState
 
     return (
         <>
+            <h2>Lista de Usuarios</h2>
 
+            {
+                users.map(user => {
+                    return (
+                        <div key={user.id}>
+                            <h3>{user.nombre} {user.apellido} </h3>
+                            <h3>{user.rut} </h3>
+                            <h3>{user.correo} </h3>
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }
