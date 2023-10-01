eed1104c3497b5e135ec33c004b59c32b63d5c39
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-01 13:11:26 -0300

    Updates Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,18 @@
-import { Headers } from "../components/Headers"
-
 import { UserList } from "../components/UserList"
-
 import { Card } from 'react-bootstrap'
 
 export const HomePage = () => {
     return (
         <>
-            <Card bg='dark text-white rounded-0'>
-                <Card.Body style={{ height: '300px', }} >
+            <Card bg='dark text-white rounded-0' className="card-fondo">
+                <Card.Body style={{ height: '500px', }} >
                     <h1>Imprimir es Facil!</h1>
-                    <p>Con la guía correcta. Transfórmate en el programador que las empresas amarán y paga sólo $49.90 el primer año.</p>
+                    <p>Con la guía correcta lograras tener tus ideas y diseños en tus manos...</p>
                 </Card.Body>
             </Card>
 
-            {/* <Headers /> */}
-
             <Card>
-                <Card.Body style={{ height: '300px' }}>
+                <Card.Body style={{ height: '500px' }}>
                     <h1>Mi nombre es</h1>
                     <h1>Raul</h1>
                     <h1>Oyarzun</h1>
@@ -25,8 +20,8 @@ export const HomePage = () => {
                 </Card.Body>
             </Card>
 
-            <Card bg='dark text-white rounded-0'>
-                <Card.Body style={{ height: '300px' }}>
+            <Card bg='dark text-white rounded-0' className="card-fondo2">
+                <Card.Body style={{ height: '500px' }}>
                     <h1>Por que imprimir en 3D?</h1>
                 </Card.Body>
             </Card>
