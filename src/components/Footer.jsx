e31cc7cef5b79b82f1fe49d9d36c2a93c97a1e6f
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-01 17:25:40 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,16 @@ import Card from 'react-bootstrap/Card';
 export const Footer = () => {
     return (
         <>
-            <Container >
-                <Card bg='dark text-white rounded-0'>
-                    <div className='text-center' style={{ height: '100px' }}>
-                        <h3>Footer</h3>
-                        <h4>Proyecto 5 - e-Commerce </h4>
-                        <h4>Creado por ROB</h4>
-                    </div>
-                </Card>
 
-            </Container>
+            <Card bg='dark text-white rounded-0'>
+                <div className='text-center' style={{ height: '100px' }}>
+                    <h3>Footer</h3>
+                    <h4>Proyecto 5 - e-Commerce </h4>
+                    <h4>Creado por ROB</h4>
+                </div>
+            </Card>
+
+
 
         </>
     )
