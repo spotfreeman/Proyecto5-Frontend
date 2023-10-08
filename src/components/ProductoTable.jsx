03a272cfee1112eb14b0272f8335c009ce3d24c3
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-08 18:07:12 -0300

    Error Imgs

--- src/components/ProductoTable.jsx
+++ src/components/ProductoTable.jsx
@@ -11,7 +11,7 @@ import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 
 //IMAGES
 import libreriaImagenes from '../assets/img/img.js'
@@ -37,6 +37,8 @@ export const ProductoTable = () => {
         setProductos(data);
     };
 
+    const imagen = 4
+
     useEffect(() => {
         getAllProductos();
     }, []);
@@ -49,9 +51,17 @@ export const ProductoTable = () => {
                         {productos.map(prod => {
                             return (
                                 <Col>
-                                    <Card key={prod._id} style={{ width: '22rem' }} className="p-2 m-2">
+                                    <Card key={prod._id} style={{ width: '30rem', backgroundColor: '#E3F9E7' }} className="p-2 m-2">
                                         <Container className='text-center'>
-                                            <h2><img src={libreriaImagenes[prod.img].img} alt="" style={{ width: '100%', borderRadius: '10px' }} /></h2>
+                                            <Image
+                                                src={libreriaImagenes[imagen].img}
+                                                alt={libreriaImagenes[0].alt}
+                                                style={{ width: '100%', borderRadius: '10px' }}
+                                                fluid
+                                            />
+
+                                            {/*
+                                            <h2><img src={libreriaImagenes[imagen].img} alt="" style={{ width: '100%', borderRadius: '10px' }} /></h2> */}
                                         </Container>
                                         <Card.Title className='text-center'> {prod.nombre}</Card.Title>
                                         <Card.Text>Cod : {prod.codigo}</Card.Text>
