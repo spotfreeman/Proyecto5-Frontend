f56b843ff5e6af609e867598f29d9d0b1d2570e7
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-14 06:17:25 -0300

    problemas con imagenes

--- src/components/ProductAdmin/ProductTable.jsx
+++ src/components/ProductAdmin/ProductTable.jsx
@@ -41,8 +41,8 @@ export const ProductTable = () => {
                                             <Card.Body>
                                                 <Row>
                                                     <Col className="text-center">
-                                                        {/* <img src={libreriaImagenes[prod.img].img} alt="Imagen de DB" height='300' /> */}
-                                                        <img src={print} alt="Imagen de DB" height='300' />
+                                                        <img src={libreriaImagenes[prod.img].img} alt="Imagen de DB" height='300' />
+                                                        {/* <img src={print} alt="Imagen de DB" height='300' /> */}
                                                     </Col>
                                                     <Col className="text-center" style={{ backgroundColor: '#E3F9E7' }}>
                                                         <h3 style={{ height: '100px' }}>{prod.material}</h3>
