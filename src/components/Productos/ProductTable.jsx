5cbc8bee8f4b04e58be9bf67bdaf9e184da57aab
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-14 05:17:41 -0300

    Imagenes MongoDb INT32 y libreriaImagenes

--- src/components/Productos/ProductTable.jsx
+++ src/components/Productos/ProductTable.jsx
@@ -3,15 +3,7 @@ import ProductoContext from "../../context/Producto/ProductoContext"
 import { Button, Card, Container, Row, Col } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 
-
-import img001 from '../../assets/img/productos/img001.jpg'
-import img002 from '../../assets/img/productos/img002.jpg'
-import img003 from '../../assets/img/productos/img003.jpg'
-import img004 from '../../assets/img/productos/img004.jpg'
-
 import libreriaImagenes from '../../assets/img/img.js'
-import print from '../../assets/img/productos/img001.jpg'
-
 
 export const ProductTable = () => {
 
@@ -50,7 +42,7 @@ export const ProductTable = () => {
                                                 <Row>
                                                     <Col className="text-center">
                                                         <img src={libreriaImagenes[prod.img].img} alt="Imagen de DB" height='300' />
-                                                        <img src={print} alt="Imagen de DB" height='300' />
+                                                        {/* <img src={print} alt="Imagen de DB" height='300' /> */}
                                                     </Col>
                                                     <Col className="text-center" style={{ backgroundColor: '#E3F9E7' }}>
                                                         <h3>{prod.descripcion}</h3>
